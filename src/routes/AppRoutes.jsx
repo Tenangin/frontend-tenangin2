0eb87526089f0d3f6ef93089ad4e087aaa6981d2
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-06-01 18:31:05 +0700

    Update: Approutes path Chatbot

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,7 +4,7 @@ import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
-import Chatbot from '../pages/chatbot';
+import Chatbot from '../pages/Chatbot';
 import Journaling from '../pages/Journaling';
 import PrivateRoute from '../components/PrivateRoute';
 
