bcc0cb0550f4adf23eb87dfe14e30fe9d253b4e0
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-06-04 18:08:27 +0700

    Update AuthCallback

--- src/pages/AuthCallback.jsx
+++ src/pages/AuthCallback.jsx
@@ -6,20 +6,24 @@ function AuthCallback() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const token = params.get("token");
+    // Parse URL hash instead of search for OAuth tokens
+    const hash = window.location.hash.substring(1); // remove leading '#'
+    const params = new URLSearchParams(hash);
+    console.log("AuthCallback params:", params.toString());
+    const token = params.get("access_token");
     const id = params.get("id");
     const username = params.get("username");
 
-    if (token && id && username) {
+    if (token) {
       setToken(token);
       setUserId(id);
       setUsername(username);
       navigate("/dashboard");
-    } else {
-      // If no token or user info, redirect to login
-      navigate("/login");
-    }
+    } 
+    // else {
+    // // If no token or user info, redirect to login
+    // navigate("/login");
+    // }
   }, [navigate]);
 
   return (
